4de14e77270f151cf4a4c5d94288ac2409b9d371
Christian Grøngaard <christianhg@gmail.com>
2015-08-30 16:09:34 +0200

    Initial folder/file structure

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  var dependencies = {
+
+  };
+
+  var paths = {
+    bower: './bower_components',
+    build: './build/',
+    dist: './dist/',
+    src: './src/',
+  };
+
+  var files = {
+
+  };
+  
+  gulp.task('build', [
+
+  ], function() {
+
+  });
+
+  gulp.task('default', [
+    'build',
+    'watch'
+  ], function() {
+
+  });
+
+  gulp.task('dist', [
+
+  ], function() {
+
+  });
+
+  gulp.task('watch', [
+    'build'
+  ], function() {
+
+  });
+})();
